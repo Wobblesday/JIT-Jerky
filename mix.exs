0a817d2ca7874a894e5193b9946906002385e9c2
Daniel Vu <daniel.vu@mailbox.org>
2020-08-16 19:31:57 -0400

    Opt into system metrics

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule JITJerky.MixProject do
   def application do
     [
       mod: {JITJerky.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
